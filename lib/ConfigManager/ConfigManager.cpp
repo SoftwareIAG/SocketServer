171b3c3e5106a3ab246d648b2891892458e22a72
Ian Torres <iantorres@outlook.com>
2021-03-05 20:20:26 -0300

    ConfigManager library fixed.

--- lib/ConfigManager/ConfigManager.cpp
+++ lib/ConfigManager/ConfigManager.cpp
@@ -5,19 +5,14 @@
  *
  */
 void ConfigManager::initialize() {
-    std::cout << "[ConfigManager::initialize] Config Manager will be initialized ... " << std::endl;
     std::ifstream file("config.cfg");
     if (!file.good())
         perror("File 'config.cfg' doesn't not exists in executable path");
     std::string key;
     std::string value;
     while (file >> key >> value) {
-        char* char_key = const_cast<char*>(key.c_str());
-        char* char_value = const_cast<char*>(value.c_str());
-        std::cout << key << " " << value << std::endl;
-        configTable.insert(char_key, char_value);
+        values[key] = value;
     }
-    std::cout << "[ConfigManager::initialize] Config Manager was initialized ... " << std::endl;
 }
 
 /**
@@ -26,11 +21,6 @@ void ConfigManager::initialize() {
  * @param key
  * @return
  */
-char* ConfigManager::get(const std::string& key) {
-    std::cout << "[ConfigManager::get] Config Manager will be get value ... " << std::endl;
-    char* char_key = const_cast<char*>(key.c_str());
-    char* char_value = configTable.get(char_key).value;
-    std::cout << char_value;
-    std::cout << "[ConfigManager::get] Config Manager will be get value ... " << std::endl;
-    return char_value;
+std::string ConfigManager::get(std::string key) {
+    return values[key];
 }
--- test.cpp
+++ test.cpp
@@ -12,6 +12,10 @@
  * @return int
  */
 int main(int argc, char *argv[]) {
+    // ConfigManager Testing
     configManager->initialize();
     assert(configManager->get("ENV") == "LOCAL");
+    std::cout << "configManager->get('ENV') returns 'LOCAL' as expected." << std::endl;
+    assert(configManager->values["ENV"] == "LOCAL");
+    std::cout << "configManager->values['ENV'] returns 'LOCAL' as expected." << std::endl;
 }
