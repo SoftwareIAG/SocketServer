4efbd7c0c4224ea65375dbbf625371df6b759398
Ian Torres <iantorres@outlook.com>
2021-03-06 23:33:14 -0300

    Cannot stat *.a fixed.

--- scripts/compile-gtest-suit.sh
+++ scripts/compile-gtest-suit.sh
@@ -3,6 +3,5 @@
 cd /usr/src/gtest || exit 1
 sudo cmake CMakeLists.txt
 sudo make
-sudo cp *.a /usr/lib
 sudo ln -s /usr/lib/libgtest.a /usr/local/lib/libgtest.a
 sudo ln -s /usr/lib/libgtest_main.a /usr/local/lib/libgtest_main.a
